2e7f47e6cd322364ab4ff817cb004d083dd84272
Alexandre Peyroux <alex@px.io>
2020-10-01 10:52:40 +0200

    msglint: init at 1.04

---
+++ default.nix
@@ -0,0 +1,17 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "msglint-1.04";
+
+  setSourceRoot = "sourceRoot=`pwd`";
+  
+  src = fetchurl {
+    url = "https://tools.ietf.org/tools/msglint/msglint-1.04.tar.gz";
+    sha256 = "1yasj6nijip3z86m526bsssd5vgqrx233azvpz1c0z564wngvzbm";
+  };
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp msglint $out/bin/msglint
+  '';
+}
